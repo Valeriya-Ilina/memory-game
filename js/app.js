551b6589efaa8a50842665fe1ebaa9573067c4a8
Valeriya Ilina <valeriya.ilina89@gmail.com>
2021-02-05 20:23:29 -0500

    timer is working

--- js/app.js
+++ js/app.js
@@ -1,13 +1,49 @@
 console.log("Memory Game1")
 
-$(() => {
-	//create 15 cards
-	for (let i = 0; i < 15; i++) {
-		//create div for the main page
-		const $div = $('<div>')
+//create cards function
+const createCard = () => {
+	//create div for the main page
+	const $div = $('<div>')
 
-		//add a class card to the div
-		$div.addClass('card')
+	//add a class card to the div
+	$div.addClass('card')
+	return $div
+}
+
+//convert timer format to seconds
+const convertTimerToSeconds = (timer) => {
+	//get minutes from string and convert to number
+	const minutes = parseInt(timer.substring(0, 2))
+	//get seconds from string and convert to number
+	const seconds = parseInt(timer.substring(3, 5))
+	// return total in seconds
+	return minutes * 60 + seconds
+
+}
+
+//convert seconds to timer format
+const convertSecondsToTimer = (seconds) => {
+	//get remainder from total seconds, e.g. 122 % 60 = 2
+	let timerSeconds = seconds % 60
+	//get total minutes, e.g. 120 / 60 = 2
+	let timerMinutes = Math.floor((seconds - timerSeconds) / 60)
+
+	//add 0 to the beginning to match timer format, e.g 01:29
+	if(timerSeconds < 10) {
+		timerSeconds = '0' + timerSeconds
+	}
+
+	if(timerMinutes < 10) {
+		timerMinutes = '0' + timerMinutes
+	}
+	return `${timerMinutes}:${timerSeconds}`
+}
+
+$(() => {
+	//create 15 cards for the 1st page
+	for (let i = 0; i < 15; i++) {
+
+		const $div = createCard()
 
 		//append div to the container
 		$div.appendTo('#container')
@@ -53,6 +89,45 @@ $(() => {
 		//when user clicks on "start game" the 1 st page should change
 		$('#first-page').detach()
 
-		alert(`Welcome ${name}! Get ready to play!`)
+		//Welcome "player"
+		//alert(`Welcome ${name}! Get ready to play!`)
+
+		//create timer text
+		const $divTimerBox = $('<div>').attr('id', 'timer')
+
+		const $div = $('<div>').text('TIMER')
+		$div.appendTo($divTimerBox)
+
+		const timer = '00:45'
+		const $divTime = $('<div>').text(timer)
+		$divTime.appendTo($divTimerBox)
+
+		$divTimerBox.prependTo('#game')
+
+		//create timer count down
+		$divTimerBox.on('click', () => {
+			
+			let timeInSeconds = convertTimerToSeconds(timer)
+			
+			let interval = setInterval(() => {
+				timeInSeconds--
+				const timer = convertSecondsToTimer(timeInSeconds)
+				$divTime.text(timer)
+
+				//if time is out, stop timer
+				if(!timeInSeconds) {
+					clearInterval(interval)
+				}
+			}, 1000)
+		})
+
+
+		//create 32 cards for the game
+		for (i = 0; i < 32; i++){
+			const $div = createCard()
+
+			$div.appendTo('#game-container')
+		}
+		//
 	})
 })
